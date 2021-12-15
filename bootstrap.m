fa03b93dcf9a679f98e62a5ff03b1ba7629eb58d
marcell.bartos <bartos.marcell24@gmail.com>
2021-12-15 20:30:49 +0100

    init and bearing angles

--- bootstrap.m
+++
@@ -1,7 +0,0 @@
-function P_0, X_0 = bootstrap(img0, img1)
-% BOOTSTRAP returns the pointcould and keypoint image coordinates from two
-% initialization images, img0 and img1
-% Returns
-% P_0, a 2*M matrix of image coordinates on img0
-% X_0, a 3*M matrix of 3D positions, the triangulated poincloud
-end
--- refresh_keypoints.m
+++ refresh_keypoints.m
@@ -3,22 +3,42 @@ function S_i = refresh_keypoints(S_i, Twc_i, params)
 % bearing angle exceeds params.min_bearing_angle.
 
 % get field of S_i to avoid unnecessary clutter
-P_i = S_i.P_i; %..etc
+P_i = S_i.P_i;
+X_i = S_i.X_i;
+C_i = S_i.C_i;
+F_i = S_i.F_i;
+Tau_i = S_i.Tau_i;
 
+M = size(C_i,2);
 % calculate bearing_angles, an 1*M matrix containing bearing angles between
 % each F_i,Tau_i (first view) pair and the current C_i, Twc_i
-bearing_angles = ... ;
+Rwc_i = Twc_i(1:3,1:3); %current rotation matrix, 3*3
+Cw_i = Rwc_i*[C_i; ones(1,M)]; %current keypoint vectors in world frame, 3*M
+
+Fw_i = zeros(3,M); %first appearance keypoint vectors in world frame, 3*M
+for i = 1:M
+    R_Tau_i = [Tau_i(1:3,i), Tau_i(4:6,i), Tau_i(7:9,i)]; %first appearance rotation matrix, 3*3
+    Fw_i(:,i) = R_Tau_i*[F_i(i); 1];
+end
+
+cos_angle = sum(Cw_i.*Fw_i)/(vecnorm(Cw_i).*vecnorm(Fw_i)); %cosine of angles using inner product, 1*M
+bearing_angles = acos(cos_angle); %angles, 1*M
+
 % select the indices which we will triangulate
 triangluation_indices = bearing_angles > params.min_bearing_angle;
 % triangulate points and get the new X_i, P_i values
 X_new = triangulate_points(C_i, F_i, Tau_i, Twc_i, triangluation_indices);
 X_i = [X_i X_new];
-P_i = [P_i, C_i(:,triangluation_indices)]
+P_i = [P_i, C_i(:,triangluation_indices)];
 % discard the added points from C_i, F_i, Tau_i
 C_i = C_i(:, ~triangluation_indices);
 F_i = F_i(:, ~triangluation_indices);
 Tau_i = Tau_i(:, ~triangluation_indices);
 
 % set fields of the output S_i
-S_i.P_i = P_i %...etc
+S_i.P_i = P_i;
+S_i.X_i = X_i;
+S_i.C_i = C_i;
+S_i.F_i = F_i;
+S_i.Tau_i = Tau_i;
 end
